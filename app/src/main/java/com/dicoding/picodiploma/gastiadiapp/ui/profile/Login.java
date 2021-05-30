6f4d2a11c542e923303eb1bf220cbdf68c06fa36
CindyKlaudia <cindyklaudia90@gmail.com>
2021-05-30 16:40:44 +0700

    Menambahkan gambar dashboard Menambahkan Teks judul Menambahkan Teks Deskripsi Membuat Login Membuat Register

--- app/src/main/java/com/dicoding/picodiploma/gastiadiapp/ui/profile/Login.java
+++ app/src/main/java/com/dicoding/picodiploma/gastiadiapp/ui/profile/Login.java
@@ -33,57 +33,5 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Email = findViewById(R.id.Email);
-        Password = findViewById(R.id.Password);
-        progressBar = findViewById(R.id.progressBar3);
-        eLogin = findViewById(R.id.btnLogin);
-        Create = findViewById(R.id.txtCreateACC);
-        mAuth = FirebaseAuth.getInstance();
-
-        eLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String email = Email.getText().toString().trim();
-                String password = Password.getText().toString().trim();
-
-                if(TextUtils.isEmpty(email)){
-                    Email.setError("Email is Required");
-                    return;
-                }
-                if(TextUtils.isEmpty(password)){
-                    Password.setError("Password is Required");
-                    return;
-                }
-                if(password.length() < 6){
-                    Password.setError("Password Must be >= 6 Characters");
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-
-                // authenticate the user
-
-                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Login.this,"Logged is Successfully.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }else {
-                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
-                    }
-                });
-
-            }
-        });
-        Create.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Register.class));
-            }
-        });
     }
 }
